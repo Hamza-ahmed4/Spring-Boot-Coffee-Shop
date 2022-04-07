49b805060471b1383d9487620dd405f6e14dc5ce
Hamza Ahmed <Hamza.Ahmed@avanza.pk>
2022-04-07 10:37:39 +0500

    Added my project

---
+++ src/main/java/com/example/springcoffeecurd/SpringcoffeecurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcoffeecurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoffeecurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoffeecurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcoffeecurd/SpringcoffeecurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcoffeecurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoffeecurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
